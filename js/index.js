94a9b812a008f000c90ce74bf2385001fc4f5047
Jazmin Amato <proyectos.amato@gmail.com>
2022-04-21 14:15:38 -0300

    Ejercicios 1 - 5

---
+++ js/index.js
@@ -0,0 +1,13 @@
+alert("Hello World"); // Ejercicio 1
+
+document.write("Hello World<br>"); // Ejercicio 2
+
+document.write((3+5) + "<br>"); // Ejercicio 3
+
+var nombreUsuario = prompt("Indique su nombre"); // Ejercicio 4
+document.write("Hola " + nombreUsuario + "<br>");
+
+var num1 = parseInt(prompt("Indique un numero")); // Ejercicio 5
+var num2 = parseInt(prompt("Indique otro numero"));
+document.write("La suma de los numeros es: " + (num1 + num2) );
+
